6610041acc1ccd73c80b3809d04da787c7f4e140
Taqi1472 <142732548+Taqi1472@users.noreply.github.com>
2023-10-21 09:56:37 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos=[];
+let loop=true;
+
+while (loop){
+    const answer:{Todo:string,AddMore:boolean}=await inquirer.prompt([{
+        type:"input",
+        name:"Todo",
+        message:"Enter your Todo List"
+    },
+    {
+        type:"confirm",
+        name:"AddMore",
+        message:"Do you want to add more Todo?",
+        default:false
+    }
+])
+const {AddMore,Todo}=answer
+console.log(answer);
+loop=AddMore;
+if (Todo){
+    todos.push(Todo)
+}
+else{
+    console.log("Kindly add valid input")
+}
+}
+console.log(todos)
